6862ab46e852cc26807ceb240fbeb11883e03535
Mukitur Reza <Mukitur Reza>
2016-10-31 05:11:50 +0600

    facebook integration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    app_id = ENV['app_id']
-    secret = ENV['secret']
+    app_id = 1259020390821500
+    secret = 6c1303bd2e2960faf351e91e6c25318b
 
     provider :facebook, app_id, secret, scope: 'publish_actions',
                 :image_size => {width: 500, height: 500}
