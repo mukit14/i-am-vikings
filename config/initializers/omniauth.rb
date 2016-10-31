909f1c42f29351863dac14aefd5c5bf8614e10ab
mukit14 <mukit@webable.com.bd>
2016-10-31 17:41:35 +0600

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], 'publish_actions',
+  provider :facebook, 1230799600310246, e468d419ec0392bb7739979a3ccfdd12, 'publish_actions',
                 :image_size => {width: 500, height: 500}
 end
