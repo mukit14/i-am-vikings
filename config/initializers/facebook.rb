1dce4426e6ce504cad6b8e109299aef270e572d4
mukit14 <mukit@webable.com.bd>
2016-10-31 17:32:45 +0600

    Update facebook.rb

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,2 +1,1 @@
-ENV = YAML.load_file("#{::Rails.root}/config/facebook.yml")[::Rails.env]
-YAML.load_file("#{::Rails.root}/config/facebook.yml")[::Rails.env]
+FACEBOOK_CONFIG = YAML.load_file("#{::Rails.root}/config/facebook.yml")[::Rails.env]
